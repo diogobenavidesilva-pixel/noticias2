7dd1a642dfeee021ec8554010435788126573f43
Diogo Benavides da Silva <1050230@senacminas.edu.br>
2026-05-14 16:40:31 -0300

    implementar funcionalidade lightbox

--- html/noticias.js
+++ html/noticias.js
@@ -3,15 +3,26 @@ function alterar_Tema() {
     body.classList.toggle('escuro');
 
     } 
-     
+
 
     if (body.classList.contains('escuro')) {
-        body.classList. remove('escuro');
-        body.classList.add('claro');
+        body.classList. remove('claro');
+        body.classList.add('escuro');
     
     }
     else {
-        body.classList.remove('claro');
-        body.classList.add('escuro');
+        body.classList.remove('escuro');
+        body.classList.add('claro');
+    }
+        
+    function abrir_lightbox(element) {
+        var lightbox = document.getElementById("lightbox");
+        var lightbox_img = document.getElementById("lightbox-img");
+
+        lightbox_img.src = element.src;
+        lightbox.style.display = "flex";
     }
     
+    function fechar_lightbox() {
+        document.getElementById("lightbox").style.display = "none";
+    }
