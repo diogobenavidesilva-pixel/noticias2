e3c496a0f7371ab871c727423e4230ef40af6646
Diogo Benavides da Silva <1050230@senacminas.edu.br>
2026-05-18 15:19:40 -0300

    estamos trabalhando nisso

--- html/noticias.js
+++ html/noticias.js
@@ -26,3 +26,32 @@ function alterar_Tema() {
     function fechar_lightbox() {
         document.getElementById("lightbox").style.display = "none";
     }
+
+    function exibir_noticias() {
+    const section = document.getElementById('noticias');
+    }
+
+    var noticias = [
+        {
+            imagem: "Designer-UI.jpg",
+            titulo: "técnicas de UI/UX pra desenvolvedores Júnior",
+            descricao: "Assim mesmo, o consenso sobre a necessidade de qualificação auxilia a preparação e a composição dos relacionamentos verticais entre as hierarquias. Vale destacar que a gestão eficiente dos recursos estratégicos garante a contribuição de um grupo importante na determinação de todos os recursos funcionais envolvidos. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a hegemonia do ambiente político faz parte de um processo de gerenciamento das rupturas provocadas pela transformação digital. O que temos que ter sempre em mente é que a adoção de políticas descentralizadoras desafia a capacidade de equalização das posturas dos órgãos dirigentes com relação às suas atribuições. ",
+            link: "../html/noticias.html"
+        }
+    ];
+    
+    var html = '';
+    noticias.forEach(noticia) 
+        html += `
+        <article class="noticia">
+            <img src="../img/Desginer-UI.jpg" onclick="abrir_lightbox(this)" alt="Designer UI" class="noticia3">
+            <h2>Notícia 3</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel sapien eget nunc efficitur commodo. Sed at ligula a nunc efficitur commodo. Sed at ligula a nunc efficitur commodo.</p>
+            <a href="#" class="btn">Ler mais</a>
+        </article>
+       .;
+    }); 
+
+    section.innerHTML = html;
+}
+        
